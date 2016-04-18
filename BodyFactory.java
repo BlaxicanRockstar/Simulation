aa177f1d8119ad883373bf45ffdbff9f26cba4c0
BlaxicanRockstar <ambercunningham94@gmail.com>
2016-04-18 14:01:27 -0400

    Update and rename BodyFactory to BodyFactory.java

---
+++ BodyFactory.java
@@ -0,0 +1,4 @@
+/* Receive body parts from a body factory and place the parts in the
+production line. The inter-arrival intervals of the body parts follow an
+exponential probability distribution with a mean inter-arrival period
+of 7 .25 time units. */
