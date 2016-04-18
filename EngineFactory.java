8ff3b56aeac34cb4e28966e47c91bc57d305e320
BlaxicanRockstar <ambercunningham94@gmail.com>
2016-04-18 14:05:00 -0400

    Update and rename EngineFactory to EngineFactory.java

---
+++ EngineFactory.java
@@ -0,0 +1,4 @@
+/* Receive the engines and place them in the production 
+line. The interarrival intervals of the engines follow an 
+exponential probability distribution, with a mean inter-arrival 
+period of 6 .15 time units. */
