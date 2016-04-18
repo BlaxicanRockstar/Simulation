bee42d25c21d7130844efa2841180173f36aed8e
BlaxicanRockstar <ambercunningham94@gmail.com>
2016-04-18 14:00:26 -0400

    Create BodyPaint.java

---
+++ BodyPaint.java
@@ -0,0 +1,5 @@
+/* Assemble the body and paint of those body parts available in the
+production line. The interval to carry out the activities in this stage
+follows a normal probability distribution with a mean period of 5 .25
+time units and a standard deviation of 1 .10 time units. There are two
+stations in the plant that carry out this stage of production. */
