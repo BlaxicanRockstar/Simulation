7cae8bc2cbc185d94d915c5a67242adf20f9d0e5
BlaxicanRockstar <ambercunningham94@gmail.com>
2016-04-18 14:09:25 -0400

    Create Ship.java

---
+++ Ship.java
@@ -0,0 +1,5 @@
+/* Ship five automobiles per truck to final destination. This stage takes
+five completed automobiles and places them on a truck for delivery.
+The interval that this stage takes follows a normal distribution with a
+mean interval of 4 .20 time units and a standard deviation of 1 .12 time
+units. */
