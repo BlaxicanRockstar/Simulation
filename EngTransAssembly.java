12a2b833ace6dfe7c714100f3cf931109e7bc465
BlaxicanRockstar <ambercunningham94@gmail.com>
2016-04-18 14:07:02 -0400

    Create EngTransAssembly.java

---
+++ EngTransAssembly.java
@@ -0,0 +1,5 @@
+/* Install engine and transmission from the units taken from the production 
+line. The interval to perform these activities follows a normal
+probability distribution with a mean interval of 7 .05 time units and
+a standard deviation of one 1 .15 time units. There are three stations
+that carry out this stage. */
