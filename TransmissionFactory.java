8125249e11dcd73d29304b7d8dfd0296647889d1
BlaxicanRockstar <ambercunningham94@gmail.com>
2016-04-18 14:05:36 -0400

    Update and rename TransmissionFactory to TransmissionFactory.java

---
+++ TransmissionFactory.java
@@ -0,0 +1,3 @@
+/* Receive the transmissions and place them in the production line. The
+inter-arrival intervals of the transmissions follow an exponential probability 
+distribution with a mean inter-arrival period of 9 .12 time units. */
