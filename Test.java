5e2b4fb2bcaeba4e1d27a45853112ce1737b99e0
BlaxicanRockstar <ambercunningham94@gmail.com>
2016-04-18 14:08:45 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,4 @@
+/* Final inspection and test automobile units. This stage places a completed 
+and tested automobile in the production line after inspection
+and test. The interval to perform these activities is a constant value
+of 16 .75 time units. */
